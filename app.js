92aac3f7950375f7c9318bbc6f3cad2879a1bb13
John Christopher Ilacad <johnchristopherilacad27@gmail.com>
2023-01-22 01:06:06 +0800

    change path

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ passport.deserializeUser(function (user, cb) {
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets"
+        callbackURL: "https://secrets-rho.vercel.app/auth/google/secrets"
     },
     function (accessToken, refreshToken, profile, cb) {
         User.findOrCreate({googleId: profile.id}, function (err, user) {
@@ -94,7 +94,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
         clientID: process.env.APP_ID,
         clientSecret: process.env.APP_SECRET,
-        callbackURL: "http://localhost:3000/auth/facebook/secrets"
+        callbackURL: "https://secrets-rho.vercel.app/auth/facebook/secrets"
     },
     function (accessToken, refreshToken, profile, cb) {
         User.findOrCreate({facebookId: profile.id}, function (err, user) {
